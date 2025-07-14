a7cef5c5ff44da4bb83cb9920ca7fe261b8fc2fe
Om Sharma <omsharma.83173@gmail.com>
2025-07-14 17:24:06 +0530

    feat : added-todo-app

---
+++ my-app/app/(tabs)/todo.tsx
@@ -0,0 +1,37 @@
+import { View,TextInput,Text,TouchableOpacity } from "react-native";
+import { styles } from "../../styles/styles"
+import { useState } from "react";
+export default function ToDo(){
+    const [todos,setTodos] = useState<any>([]);
+    const [todo,setTodo] = useState<any>();
+    function addTodo(){
+        setTodos([...todos,todo]);
+        setTodo("");
+    }
+
+    function handleDeleteTodo(deleteTodo:any){
+        const updatedTodos = todos.filter((todo:any)=> todo !== deleteTodo);
+        setTodos(updatedTodos);
+    }
+    return <View style={styles.container}>
+        <TextInput placeholder="Enter Todo" onChangeText={(text)=>{setTodo(text)}} style={styles.input}></TextInput>
+        <TouchableOpacity style={styles.button} onPress={addTodo}>
+            <Text style={styles.buttonText}>Add Todo</Text>
+        </TouchableOpacity>
+        
+        <Text style={styles.heading}> All Todos!</Text>
+        {todos && todos.length > 0 ? 
+        <>
+            {todos.map((todo:any)=>{
+                return <>
+                <Text style={styles.text}>{todo}</Text>
+                <TouchableOpacity  onPress={()=>handleDeleteTodo(todo)}>
+                <Text>❌</Text>
+            </TouchableOpacity>
+                </>
+            })}
+        </>
+        : ""}
+        
+    </View>
+}
