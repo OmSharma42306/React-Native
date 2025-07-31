c7665ad61a009233a91a52bfdf318b4d7b7e64cb
Om Sharma <omsharma.83173@gmail.com>
2025-07-31 14:51:21 +0530

    feat : added-more-components.

--- my-app/lib/axios.ts
+++ my-app/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL : `http://localhost:3000/user`,
+    baseURL : `http://192.168.29.226:3000/user`,
     headers : {
         "Content-Type": "application/json"
     },
